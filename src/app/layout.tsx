aafedfe91c57c3d0f40429992e3c5e8f88007030
Pablo George <pablogeokar@hotmail.com>
2024-01-20 16:01:49 -0300

    v0.0.69

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "../../node_modules/highlight.js/styles/dracula.css";
-//import '../../node_modules/zionix-ui/dist/style.css'
 
 export const metadata: Metadata = {
   title: "ZIONIX ui",
